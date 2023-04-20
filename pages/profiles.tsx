5e26f2ed16eab21139574578d639b60d9c03fd5e
Manish Bajagai <manishbajagi2@gmail.com>
2023-04-20 18:21:37 +0530

    Navbar completed

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,8 +1,6 @@
-import useCurrentUser from "@/hooks/useCurrentUser"
+import Navbar from "@/components/Navbar"
 import { NextPageContext } from "next"
 import { getSession } from "next-auth/react"
-import Image from "next/image"
-import { useRouter } from "next/router"
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context)
@@ -22,36 +20,8 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 const Profiles = () => {
-    const router = useRouter()
-    const { data: user } = useCurrentUser()
     return (
-        <div className="flex items-center h-full justify-center">
-            <div className="flex flex-col">
-                <h1 className="text-3xl md:text-6xl text-white text-center">
-                    Who&#39;s watching?
-                </h1>
-                <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={() => router.push('/')}>
-                        {/* <UserCard name={currentUser?.name} /> */}
-                        <div className="group flex-row w-44 mx-auto">
-                            <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
-                                <Image
-                                    draggable={false}
-                                    className="h-max object-contain"
-                                    src="/images/default-blue.png"
-                                    alt="Profile Images"
-                                    width={180}
-                                    height={180}
-                                />
-                            </div>
-                            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-                                {user?.name}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Navbar />
     )
 }
 export default Profiles
