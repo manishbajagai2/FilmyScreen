acecea220a7a94969cb96bb7442b2281a06577d9
Manish Bajagai <manishbajagi2@gmail.com>
2023-04-24 20:28:24 +0530

    Random Profile Pic if not there in sign in

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,8 +1,21 @@
-import useCurrentUser from "@/hooks/useCurrentUser"
 import { NextPageContext } from "next"
 import { getSession } from "next-auth/react"
-import Image from "next/image"
 import { useRouter } from "next/router"
+import { useCallback } from "react"
+
+import useCurrentUser from "@/hooks/useCurrentUser"
+import Image from "next/image"
+
+const images = [
+    "/images/default-blue.png",
+    "/images/default-red.png",
+    "/images/default-slate.png",
+    "/images/default-green.png",
+]
+
+interface UserCardProps {
+    name: string
+}
 
 export async function getServerSideProps(context: NextPageContext) {
     const session = await getSession(context)
@@ -21,9 +34,36 @@ export async function getServerSideProps(context: NextPageContext) {
     }
 }
 
+const UserCard: React.FC<UserCardProps> = ({ name }) => {
+    const imgSrc = images[Math.floor(Math.random() * 4)]
+
+    return (
+        <div className="group flex-row w-44 mx-auto">
+            <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
+                <Image
+                    draggable={false}
+                    className="h-max object-contain"
+                    src="/images/default-blue.png"
+                    alt="Profile Images"
+                    width={180}
+                    height={180}
+                />
+            </div>
+            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
+                {name}
+            </div>
+        </div>
+    )
+}
+
 const Profiles = () => {
     const router = useRouter()
-    const { data: user } = useCurrentUser()
+    const { data: currentUser } = useCurrentUser()
+
+    const selectProfile = useCallback(() => {
+        router.push("/")
+    }, [router])
+
     return (
         <div className="flex items-center h-full justify-center">
             <div className="flex flex-col">
@@ -31,23 +71,8 @@ const Profiles = () => {
                     Who&#39;s watching?
                 </h1>
                 <div className="flex items-center justify-center gap-8 mt-10">
-                    <div onClick={() => router.push("/")}>
-                        {/* <UserCard name={currentUser?.name} /> */}
-                        <div className="group flex-row w-44 mx-auto">
-                            <div className="w-44 h-44 rounded-md flex items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden">
-                                <Image
-                                    draggable={false}
-                                    className="h-max object-contain"
-                                    src="/images/default-blue.png"
-                                    alt="Profile Images"
-                                    width={180}
-                                    height={180}
-                                />
-                            </div>
-                            <div className="mt-4 text-gray-400 text-2xl text-center group-hover:text-white">
-                                {user?.name}
-                            </div>
-                        </div>
+                    <div onClick={() => selectProfile()}>
+                        <UserCard name={currentUser?.name} />
                     </div>
                 </div>
             </div>
